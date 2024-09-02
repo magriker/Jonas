cb43531f6c56a4dc4219b21611773abcf8249a57
magriker <magken7@gmail.com>
2024-09-02 21:48:52 +0900

    commit

--- Jonas_React/ultimate-react-course-main/03-pizza-menu/pizza-menu/src/index.js
+++ Jonas_React/ultimate-react-course-main/03-pizza-menu/pizza-menu/src/index.js
@@ -1,13 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Header = () => {
+  // const style = { color: " red", fontSize: "48px", textTransform: "uppercase" };
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+};
+
+const Menu = () => {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza></Pizza>
+      <Pizza></Pizza>
+      <Pizza></Pizza>
+    </main>
+  );
+};
+
+const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We are currently open!");
+  // else alert("sorry wr are closed");
+
+  return (
+    <footer>{new Date().toLocaleTimeString()}. we are currently open</footer>
+  );
+};
+
+const Pizza = () => {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"></img>
+      <h3>Pizza Spinaci</h3>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="container">
+      <Header></Header>
+      <Menu></Menu>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App></App>
   </React.StrictMode>
 );
 
