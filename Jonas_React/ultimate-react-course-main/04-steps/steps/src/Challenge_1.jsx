6c466d35149065756172f7e6bb245e47f90aa734
magriker <magken7@gmail.com>
2024-09-12 21:43:05 +0900

    steps

---
+++ Jonas_React/ultimate-react-course-main/04-steps/steps/src/Challenge_1.jsx
@@ -0,0 +1,13 @@
+const Challenge_1 = () => {
+  return (
+    <div className="challenge_1">
+      <Counter></Counter>
+    </div>
+  );
+};
+
+const Counter = () => {
+  return <div>Test</div>;
+};
+
+export default Challenge_1;
