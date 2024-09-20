3ceb9a854693970b4fae7479d11be9351d2ed498
magriker <magken7@gmail.com>
2024-09-20 23:58:16 +0900

    challenge

--- Jonas_React/ultimate-react-course-main/04-steps/travel-list/src/index.js
+++ Jonas_React/ultimate-react-course-main/04-steps/travel-list/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Challenge_1 from "./Challenge_1";
+import Challenge_2 from "./Challenge_2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Challenge_2></Challenge_2>
+    {/* <App /> */}
   </React.StrictMode>
 );
